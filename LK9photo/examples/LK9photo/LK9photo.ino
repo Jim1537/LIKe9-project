e19e7258ae8ea5268a0fe722ded259508db15f43
Jim1537 <67700443+Jim1537@users.noreply.github.com>
2020-07-23 09:19:10 -0500

    Add files via upload

--- LK9photo/examples/LK9photo/LK9photo.ino
+++ LK9photo/examples/LK9photo/LK9photo.ino
@@ -11,34 +11,55 @@
 #include "LK9photo.h"
 
 /* Define some constants...
- 
+
     Note:
-    You do not need to define time interval constant below if you are going to use its default value.
+    You do not need to define these constants below if you are going to use its default values.
 */
 #define PHR_INTERVAL 3000   // Time interval between data collecting (millis; default is 3000 = 3 sec.)
 #define PHR_PIN A1          // Photoresistor control pin set as A1 (Any available analog pin can be used)
+#define PHR_LIMIT_MIN 0     // Minimum value to constrain readings (default is 0).
+#define PHR_LIMIT_MAX 1023  // Maximum value to constrain readings (default is 1023).
 
 // Declare your photoresistor as 'dev1' object:
 LK9photo dev1(PHR_PIN);
-/* Note:
+/*
   You can add as many photoresistors as you need (as long as you have enough analog signal pins on the board).
-  Each photoresistor may have its own timer.
-    LK9photo dev2(PHR_PIN2);
-    LK9photo dev3(PHR_PIN3);
-    LK9photo dev4(PHR_PIN4);
-    etc...
+  Each photoresistor will have its own timer and other settings.
+  
+      LK9photo dev2(PHR_PIN2);
+      LK9photo dev3(PHR_PIN3);
+      LK9photo dev4(PHR_PIN4);
+      etc...
 */
 
 void setup() {
   Serial.begin(115200);
 
   /*
-    You can DO NOTHING here!
-    In this case, the time interval will be taken by its default value (see the description of the constants above).
+    Actually, you can DO NOTHING here!
+    In this case, the time interval and other parameters will be taken by their default values (see the description of the constants above).
 
     But if you want to set your own specific time interval, you can do it (for each photoresistor declared above):
   */
   dev1.set(PHR_INTERVAL);
+
+  /*
+    By default, all the readings are constrained by full range of signal between 0 and 1023.
+    But in real life this range more narrow. To increase the resolution of results you are able to set
+    the actual range of reading.
+
+        dev1.set(PHR_LIMIT_MIN, PHR_LIMIT_MAX);
+
+    In this case you will get a sample of values ​​from a smaller range, which will increase
+    the resolution of final readings.
+  */
+
+  /*
+    Also, you can set all the parameters by one call (time interval and constraints):
+
+        dev1.set(PHR_INTERVAL, PHR_LIMIT_MIN, PHR_LIMIT_MAX);
+  */
+
 }
 
 void loop() {
@@ -49,14 +70,18 @@ void loop() {
 
     It is possible to override internal timer sending boolean 'true' as the parameter to the function.
     In this case, internal timer will be bypassed and all the data will be collected immediately. Timer will be reseted to the time of last function call:
-    dev1.update(true);
+
+        dev1.update(true);
   */
 
   // Update readings and check if data collected...
   if (dev1.update()) {
-    /* If so, then you can get the values ​​by accessing the functions:
-        get()
-        Function returns the value between 0 (low light level) and 255 (high light level).
+    /*
+      If so, then you can get the values ​​by accessing the functions:
+
+          dev1.get();
+
+      Function returns the value between 0 (low light level) and 255 (high light level).
     */
     Serial.print("Light level:\t");
     Serial.println(dev1.get());
